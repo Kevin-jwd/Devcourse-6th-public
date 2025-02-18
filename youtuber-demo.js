a08e3328cfde55251ae1df4c107db1d7ad83584d
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-02-18 19:15:34 +0900

    feat: youtuber-demo.js 추가
    - private 저장소에 있던 youtuber-demo.js 복사

---
+++ youtuber-demo.js
@@ -0,0 +1,55 @@
+// 서버 객체 생성
+const express = require("express");
+const app = express();
+
+// 포트 1234에 서버 실행
+app.listen(1234);
+
+// youtuber 객체 생성
+let youtuber1 = {
+    channelTitle: "육식맨",
+    subs: "130만명",
+    videoNum: "289개",
+};
+
+let youtuber2 = {
+    channelTitle: "침착맨",
+    subs: "227만명",
+    videoNum: "6600개",
+};
+
+let youtuber3 = {
+    channelTitle: "김진짜",
+    subs: "83만명",
+    videoNum: "600개",
+};
+
+let db = new Map();
+
+db.set(1, youtuber1);
+db.set(2, youtuber2);
+db.set(3, youtuber3);
+
+// GET
+app.get("/youtuber/:id", function (req, res) {
+    let { id } = req.params;
+    id = parseInt(id);
+    const youtuber = db.get(id);
+
+    if (youtuber == undefined) {
+        res.json({
+            message: "존재하지 않는 유튜버입니다.",
+        });
+    } else {
+        res.json(youtuber);
+    }
+});
+
+// POST
+app.use(express.json());
+app.post("/youtuber", function (req, res) {
+    db.set(4, req.body);
+    res.json({
+        message: `${db.get(4).channelTitle}님, Youtube 채널 개설을 축하드립니다!`,
+    });
+});
