93b17ecd92ab6bf3665f96d0d7a0f6fdf61b15df
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-03-04 13:03:12 +0900

    feat: 사용자 API 입력값 검증 추가
    - 사용자 개별 조회, 사용자 삭제 이메일 입력값 검증
    - 사용자 로그인 이메일/비밀번호 검증
    - 사용자 회원가입 이메일/이름/비밀번호/연락처 검증

--- youtube-demo/routes/user-demo.js
+++ youtube-demo/routes/user-demo.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mysql = require("mysql2");
+const { body, param, validationResult } = require("express-validator");
 const router = express.Router();
 
 router.use(express.json());
@@ -12,68 +13,158 @@ const conn = mysql.createConnection({
     database: "Youtube",
 });
 
-// 사용자 조회 (GET /users)
-router.get("/users", (req, res) => {
-    const { email } = req.body;
-
-    conn.query(
-        `SELECT * FROM users WHERE users.email = ?`,
-        [email],
-        function (error, results) {
-            if (error) {
-                return res
-                    .status(500)
-                    .json({ message: "서버 오류가 발생했습니다." });
-            }
-            if (results.length != 0) {
-                return res.status(200).json(results);
-            } else {
-                return res.status(404).json({
-                    message: "존재하지 않는 사용자입니다.",
-                });
-            }
-        }
-    );
-});
-
-// 로그인 (POST /login)
-router.post("/login", (req, res) => {
-    const { email, password } = req.body;
-    let loginUser = {};
-
-    conn.query(
-        `SELECT * FROM users WHERE users.email = ?`,
-        [email],
-        function (error, results) {
-            if (error) {
-                return res
-                    .status(500)
-                    .json({ message: "서버 오류가 발생했습니다." });
-            }
-            if (results.length !== 0 && results[0].password === password) {
-                return res.status(200).json({
-                    message: `${results[0].name}님 성공적으로 로그인되었습니다.`,
-                });
-            } else if (results.length !== 0) {
-                return res.status(400).json({
-                    message: "비밀번호가 틀렸습니다.",
-                });
-            } else {
-                return res.status(404).json({
-                    message: "존재하지 않는 사용자입니다.",
-                });
-            }
-        }
-    );
-});
-
-// 회원가입 (POST /register)
-router.post("/register", (req, res) => {
-    if (Object.keys(req.body).length === 0) {
+const validate = (req, res, next) => {
+    const err = validationResult(req);
+    if (!err.isEmpty()) {
         return res.status(400).json({
             message: "입력 값을 다시 한 번 확인해주세요.",
+            errors: err.array(),
         });
-    } else {
+    }
+    next();
+};
+
+router
+    .route("/users")
+    // 사용자 개별 조회 (GET /users)
+    .get(
+        [
+            body("email")
+                .notEmpty()
+                .withMessage("email은 필수 입력값입니다.")
+                .isEmail()
+                .withMessage("이메일 형식이 아닙니다."),
+            validate,
+        ],
+        (req, res) => {
+            const { email } = req.body;
+
+            conn.query(
+                `SELECT * FROM users WHERE users.email = ?`,
+                [email],
+                function (error, results) {
+                    if (error) {
+                        return res
+                            .status(500)
+                            .json({ message: "서버 오류가 발생했습니다." });
+                    }
+                    if (results.length != 0) {
+                        return res.status(200).json(results);
+                    } else {
+                        return res.status(404).json({
+                            message: "존재하지 않는 사용자입니다.",
+                        });
+                    }
+                }
+            );
+        }
+    )
+    // 사용자 삭제 (DELETE /users)
+    .delete(
+        [
+            body("email")
+                .notEmpty()
+                .withMessage("email은 필수 입력값입니다.")
+                .isEmail()
+                .withMessage("이메일 형식이 아닙니다."),
+            validate,
+        ],
+        (req, res) => {
+            const { email } = req.body;
+
+            conn.query(
+                `DELETE FROM users WHERE email = ?`,
+                [email],
+                (error, results) => {
+                    if (error) {
+                        return res
+                            .status(500)
+                            .json({ message: "서버 오류가 발생했습니다." });
+                    }
+
+                    if (results.affectedRows === 0) {
+                        return res
+                            .status(404)
+                            .json({ message: "존재하지 않는 사용자입니다." });
+                    }
+
+                    return res.status(200).json({
+                        message: "회원 정보 삭제가 완료되었습니다.",
+                    });
+                }
+            );
+        }
+    );
+
+// 로그인 (POST /login)
+router.post(
+    "/login",
+    [
+        body("email")
+            .notEmpty()
+            .withMessage("email은 필수 입력값입니다.")
+            .isEmail()
+            .withMessage("이메일 형식이 아닙니다."),
+        body("password")
+            .notEmpty()
+            .withMessage("비밀번호는 필수 입력값입니다."),
+    ],
+    validate,
+    (req, res) => {
+        const { email, password } = req.body;
+
+        conn.query(
+            `SELECT * FROM users WHERE users.email = ?`,
+            [email],
+            function (error, results) {
+                if (error) {
+                    return res
+                        .status(500)
+                        .json({ message: "서버 오류가 발생했습니다." });
+                }
+                if (results.length !== 0 && results[0].password === password) {
+                    return res.status(200).json({
+                        message: `${results[0].name}님 성공적으로 로그인되었습니다.`,
+                    });
+                } else if (results.length === 0) {
+                    return res.status(404).json({
+                        message: "존재하지 않는 사용자입니다.",
+                    });
+                } else {
+                    return res.status(400).json({
+                        message: "비밀번호가 틀렸습니다.",
+                    });
+                }
+            }
+        );
+    }
+);
+
+// 회원가입 (POST /register)
+router.post(
+    "/register",
+    [
+        body("email")
+            .notEmpty()
+            .withMessage("email은 필수 입력값입니다.")
+            .isEmail()
+            .withMessage("이메일 형식이 아닙니다."),
+        body("name")
+            .notEmpty()
+            .withMessage("name은 필수 입력값입니다.")
+            .isString()
+            .withMessage("이름은 문자열이어야 합니다."),
+        body("password")
+            .notEmpty()
+            .withMessage("비밀번호는 필수 입력값입니다."),
+        body("contact")
+            .notEmpty()
+            .withMessage("연락처는 필수 입력값입니다.")
+            .isString()
+            .withMessage("연락처는 문자열이어야 합니다."),
+    ],
+    validate,
+    (req, res) => {
         const { email, name, password, contact } = req.body;
         conn.query(
             `INSERT INTO users (email, name, password, contact) VALUES (?,?,?,?)`,
@@ -88,26 +179,6 @@ router.post("/register", (req, res) => {
             }
         );
     }
-});
-
-// 회원 삭제 (DELETE /users/)
-router.delete("/users", (req, res) => {
-    const { email } = req.body;
-
-    conn.query(`DELETE FROM users WHERE email = ?`, [email], (error, results) => {
-        if (error) {
-            return res.status(500).json({ message: "서버 오류가 발생했습니다." });
-        }
-
-        if (results.affectedRows === 0) {
-            return res.status(404).json({ message: "존재하지 않는 사용자입니다." });
-        }
-
-        return res.status(200).json({
-            message: "회원 정보 삭제가 완료되었습니다.",
-        });
-    });
-});
-
+);
 
 module.exports = router;
