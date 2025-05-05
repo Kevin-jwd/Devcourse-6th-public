b83d35b6662684a75c3ff9b97be3331459b73c6b
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2025-05-05 18:21:19 +0900

    feat: authorization 예제 추가

---
+++ demo-npm/authorization-demo.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+// dotenv 모듈
+const dotenv = require("dotenv");
+dotenv.config();
+
+// jwt 모듈
+const jwt = require("jsonwebtoken");
+
+app.listen(process.env.PORT);
+
+// GET + "/jwt" : 토큰 발급
+app.get("/jwt", function (req, res) {
+    let token = jwt.sign({ foo: "bar" }, process.env.PRIVATE_KEY);
+
+    res.cookie("jwt", token, {
+        httpOnly: true,
+    });
+    res.send("토큰 발급 완료");
+});
+
+// GET + "/jwt/decoded" : 토큰 검증
+app.get("/jwt/decoded", function (req, res) {
+    req.cookies
+    let decoded = jwt.verify(token, process.env.PRIVATE_KEY);
+    res.send(decoded);
+});
